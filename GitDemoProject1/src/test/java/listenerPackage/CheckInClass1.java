b81dbac26179c44ffdb09aa06280953db46008fb
sivanageswararaoboddu <32696353+sivanageswararaoboddu@users.noreply.github.com>
2021-08-02 15:11:36 -0700

    Git changes for pull request

--- GitDemoProject1/src/test/java/listenerPackage/CheckInClass1.java
+++ GitDemoProject1/src/test/java/listenerPackage/CheckInClass1.java
@@ -5,6 +5,7 @@ public class CheckInClass1 {
 	public void testmethod(){
 		System.out.println("I am in testmethod");
 		System.out.println("Adding new line for check-in");
+		System.out.println("Git changes for Pull request");
 		
 	}
 
